a04bf7d5819e4f41d09986080da04b74b0509f1e
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-19 13:55:27 +0200

    Add features, speakers & footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const burgerIcon = document.getElementById("burger-icon");
 const navMenu = document.getElementById("nav-menu");
 const closeBtn = document.querySelector(".close-btn");
+const chevron = document.querySelector(".chevron");
+const chevronBtn = document.querySelector(".show-more-speakers");
 
 burgerIcon.addEventListener("click", () => {
   navMenu.style.transform = "translateX(0)";
@@ -10,4 +12,16 @@ burgerIcon.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   navMenu.style.transform = "translateX(-100%)";
   document.body.style.overflow = "scroll";
+});
+
+chevronBtn.addEventListener("click", () => {
+  if (chevronBtn.classList.contains("active")) {
+    chevron.style.height = "40rem";
+    chevron.style.overflow = "hidden";
+    chevronBtn.classList.remove("active");
+  } else {
+    chevron.style.height = "auto";
+    chevron.style.overflow = "visible";
+    chevronBtn.classList.add("active");
+  }
 });
