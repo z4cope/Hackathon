868d2d2b8f14f0874ad859346f3208c0c8557fcd
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-19 19:56:01 +0200

    Fix images error on github pages

--- scripts/speakersData.js
+++ scripts/speakersData.js
@@ -1,42 +1,42 @@
-const imgPath = '../assets/speaker_0';
+const imgPath = "./assets/speaker_0";
 const speakers = [
   {
     image: `${imgPath}1.png`,
-    name: 'Yochai Benkler',
+    name: "Yochai Benkler",
     title:
-      'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    text: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in2006.',
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    text: "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in2006.",
   },
   {
     image: `${imgPath}2.png`,
-    name: 'Kilnam Chon',
-    title: '',
+    name: "Kilnam Chon",
+    title: "",
     text: "Kilnam Chon helped bring the Internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012, he was inducted into the inaugural class of the Internet Society's (ISOC) Internet Hall of Fame.",
   },
   {
     image: `${imgPath}3.png`,
-    name: 'SohYeong Noh',
-    title: 'Director of Art Centre Nobi and a board member of CC Korea',
-    text: 'As the main venue for new media art production in Korea, Nab promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    name: "SohYeong Noh",
+    title: "Director of Art Centre Nobi and a board member of CC Korea",
+    text: "As the main venue for new media art production in Korea, Nab promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.",
   },
   {
     image: `${imgPath}4.png`,
-    name: 'Julia Leda',
-    title: 'President of Young Pirates of Europe',
+    name: "Julia Leda",
+    title: "President of Young Pirates of Europe",
     text: "European ingetration, political democracy and participa-tion of youth through online as her major conder, Red's report outlining potential changes to EU copyright law was approved by the Parliament in July.",
   },
   {
     image: `${imgPath}5.png`,
-    name: 'Lila Tretikov',
-    title: 'Executive Director of the Wikimedia Foundation',
-    text: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
+    name: "Lila Tretikov",
+    title: "Executive Director of the Wikimedia Foundation",
+    text: "Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.",
   },
 
   {
     image: `${imgPath}6.png`,
-    name: 'Ryan Merkley',
-    title: 'CEO of Creative Commons, ex COO of the Mozilla Foundation',
-    text: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open source move-ment.',
+    name: "Ryan Merkley",
+    title: "CEO of Creative Commons, ex COO of the Mozilla Foundation",
+    text: "Ryan had been leading open-source projects at the Mozilla Foundation such as the open source move-ment.",
   },
 ];
 
