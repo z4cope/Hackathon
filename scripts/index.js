dc15d1b507ebbb7a3c871219bac8d510fd4efd70
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-19 17:01:04 +0200

    Finish about page & add some finall enhancments

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 const burgerIcon = document.getElementById("burger-icon");
 const navMenu = document.getElementById("nav-menu");
+const navItems = navMenu.getElementsByTagName("li");
 const closeBtn = document.querySelector(".close-btn");
-const chevron = document.querySelector(".chevron");
-const chevronBtn = document.querySelector(".show-more-speakers");
+
+const fader = document.getElementById("fader");
 
 burgerIcon.addEventListener("click", () => {
   navMenu.style.transform = "translateX(0)";
@@ -14,14 +15,13 @@ closeBtn.addEventListener("click", () => {
   document.body.style.overflow = "scroll";
 });
 
-chevronBtn.addEventListener("click", () => {
-  if (chevronBtn.classList.contains("active")) {
-    chevron.style.height = "40rem";
-    chevron.style.overflow = "hidden";
-    chevronBtn.classList.remove("active");
-  } else {
-    chevron.style.height = "auto";
-    chevron.style.overflow = "visible";
-    chevronBtn.classList.add("active");
-  }
+addEventListener("DOMContentLoaded", () => {
+  fader.style.transform = "translateY(100%)";
+});
+
+Array.prototype.slice.call(navItems).forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.style.transform = "translateX(-100%)";
+    document.body.style.overflow = "scroll";
+  });
 });
