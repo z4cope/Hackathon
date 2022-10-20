60a872d1fcf88cebae51f12013c08aa9c17ef844
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-20 16:50:52 +0200

    Fix reviewer notices

--- scripts/chevronLogic.js
+++ scripts/chevronLogic.js
@@ -1,14 +1,16 @@
-const chevron = document.querySelector('.chevron');
-const chevronBtn = document.querySelector('.show-more-speakers');
+const chevron = document.querySelector(".chevron");
+const chevronBtn = document.querySelector(".show-more-speakers");
 
-chevronBtn.addEventListener('click', () => {
-  if (chevronBtn.classList.contains('active')) {
-    chevron.style.height = '40rem';
-    chevron.style.overflow = 'hidden';
-    chevronBtn.classList.remove('active');
+chevronBtn.addEventListener("click", () => {
+  if (chevronBtn.classList.contains("active")) {
+    chevron.style.height = "40rem";
+    chevron.style.overflow = "hidden";
+    chevronBtn.classList.remove("active");
+    chevronBtn.getElementsByTagName("span")[0].innerText = "MORE";
   } else {
-    chevron.style.height = 'auto';
-    chevron.style.overflow = 'visible';
-    chevronBtn.classList.add('active');
+    chevron.style.height = "auto";
+    chevron.style.overflow = "visible";
+    chevronBtn.classList.add("active");
+    chevronBtn.getElementsByTagName("span")[0].innerText = "LESS";
   }
 });
